2b27f1604e51fb75890efbfc0e975b962e60474e
Gaurang <khera.gaurang@gmail.com>
2023-08-06 13:26:49 +0530

    Fix server

--- server/app.py
+++ server/app.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SECRET_KEY'] = 'super secret key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
 sess = Session()
 sess.init_app(app=app)
 
@@ -23,20 +24,11 @@ with app.app_context():
 
 @app.route("/@me")
 def get_current_user():
-    # for i in range(10):
-    #     product = Product(image="https://images.unsplash.com/photo-1526900913101-88c16676ca02?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80", category="Sneakers", name="Air Jordans", price=4000)
-    #     db.session.add(product)
-    #     db.session.commit()
     user_id = session.get("user_id")
-
     if not user_id:
         return jsonify({"error": "Unauthorized"}), 401
-    
     user = User.query.filter_by(id=user_id).first()
-    return jsonify({
-        "id": user.id,
-        "email": user.email
-    }) 
+    return jsonify({ "id": user.id, "email": user.email })
 
 @app.route('/product/<id>', methods=['GET', 'POST'])
 def return_product(id):
@@ -52,54 +44,34 @@ def register_user():
     email = request.json["email"]
     username = request.json['username']
     password = request.json["password"]
-
     email_exists = User.query.filter_by(email=email).first() is not None
     username_exists = User.query.filter_by(username=username).first() is not None
-
     if email_exists:
         return jsonify({"error": "User already exists"}), 409
-    
     if username_exists:
         return jsonify({"error": 'Username already exists.'}), 410
-    
-
     hashed_password = bcrypt.generate_password_hash(password)
     new_user = User(email=email, password=hashed_password, username=username)
     db.session.add(new_user)
     db.session.commit()
-    
     session["user_id"] = new_user.id
-
-    return jsonify({
-        "id": new_user.id,
-        "email": new_user.email
-    })
+    return jsonify({ "id": new_user.id, "email": new_user.email })
 
 @app.route("/login", methods=["POST"])
 def login_user():
     email = request.json["email"]
     password = request.json["password"]
-    print(email, password)
-
     user = User.query.filter_by(email=email).first()
-
     if user is None:
         return jsonify({"error": "Unauthorized"}), 404
-
     if not bcrypt.check_password_hash(user.password, password):
         return jsonify({"error": "Unauthorized"}), 403
-    
     session["user_id"] = user.id
-    print(session['user_id'])
-
-    return jsonify({
-        "id": user.id,
-        "email": user.email
-    })
+    return jsonify({ "id": user.id, "email": user.email })
 
 @app.route("/logout", methods=["POST"])
 def logout_user():
-    session.pop("user_id")
+    session.pop("user_id", None)
     return "200"
 
 if __name__ == "__main__":
